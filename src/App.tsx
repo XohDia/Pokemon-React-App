cae6187a80f7715f6a86cd3a71909afd5d5e4558
Xodia <hxodia@gmail.com>
2025-10-19 15:53:22 +0200

    add some node_modules

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ const App: FunctionComponent = () => {
     const name: string = "React";
 
     return (
-        <h1>Hello, {name}!</h1>
+        <h1>Bonjour, {name}!</h1>
     )
 }
 
 export default App;
+
+/*
+import React from "react";
+
+export default class App extends React.Component {
+    const name: string = "React";
+
+    render() {
+        return ( <h1>Bonjour, {name}!</h1> );
+    }
+}
+*/
