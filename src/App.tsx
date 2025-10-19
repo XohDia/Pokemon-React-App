36e4c24262c335a6d522566c80a159de12d4653a
Xodia <hxodia@gmail.com>
2025-10-19 16:59:33 +0200

    add a pokemon array and pokemon class

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, {FunctionComponent} from "react";
+import React, { FunctionComponent, useState } from "react";
 
 const App: FunctionComponent = () => {
-    const name: string = "React";
+    // const name: string = "React";
+    const [name, setName] = useState<String>('name');
 
     return (
         <h1>Bonjour, {name}!</h1>
@@ -10,12 +11,12 @@ const App: FunctionComponent = () => {
 
 export default App;
 
-/*
+/* 
 import React from "react";
 
 export default class App extends React.Component {
     const name: string = "React";
-
+ 
     render() {
         return ( <h1>Bonjour, {name}!</h1> );
     }
