6e72b4bbcb6222504843a2d58b490c350b308310
Xodia <hxodia@gmail.com>
2025-10-19 02:20:09 +0200

    making my first react app start with 'Hello React'

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from "react";
+
+const App: FunctionComponent = () => {
+    const name: string = "React";
+
+    return (
+        <h1>Hello, {name}!</h1>
+    )
+}
+
+export default App;
