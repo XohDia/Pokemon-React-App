a4323b0d1190678780813e45f5098f664f64d449
Xodia <hxodia@gmail.com>
2025-10-19 21:09:30 +0200

    add gitignore + add a hook to my function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React, { FunctionComponent, useState } from "react";
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/mock-pokemons";
 
 const App: FunctionComponent = () => {
     // const name: string = "React";
-    const [name, setName] = useState<String>('name');
+    const [name, setName] = useState<String>('Dresseur');
+    const [pokemons, setPokemons] = useState<Pokemon[]>(POKEMONS);
 
     return (
-        <h1>Bonjour, {name}!</h1>
+        <div>
+            <h1>Bonjour, {name}!</h1>
+            <h2>Pokédex</h2>
+            <p>Il y a {pokemons.length} pokémons dans le pokédex.</p>
+        </div>
     )
 }
 
